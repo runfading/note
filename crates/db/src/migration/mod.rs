77cc52b475f729692c8f354a0a8711210a4a2575
runfa <runfading@aliyun.com>
2026-06-26 10:53:56 +0800

    feat(db): migration create_notebooks

--- crates/db/src/migration/mod.rs
+++ crates/db/src/migration/mod.rs
@@ -1,9 +1,11 @@
 use sea_orm_migration::prelude::*;
 
+mod m20260626_000001_create_notebooks;
+
 pub struct Migrator;
 
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![]
+        vec![Box::new(m20260626_000001_create_notebooks::Migration)]
     }
 }
